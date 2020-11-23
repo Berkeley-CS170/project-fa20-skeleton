af9d4523914a1aea865fc80c21898323b2c2eaa0
Augustine Lee <augustinedlee@berkeley.edu>
2020-11-22 17:34:34 -0800

    saving solver

--- solver.py
+++ solver.py
@@ -1,5 +1,5 @@
 import networkx as nx
-from parse import read_input_file, write_output_file
+from parse import read_input_file, write_output_file, read_output_file
 from utils import is_valid_solution, calculate_happiness
 import sys
 
@@ -22,14 +22,13 @@ def solve(G, s):
 
 # Usage: python3 solver.py test.in
 
-# if __name__ == '__main__':
-#     assert len(sys.argv) == 2
-#     path = sys.argv[1]
-#     G, s = read_input_file(path)
-#     D, k = solve(G, s)
-#     assert is_valid_solution(D, G, s, k)
-#     print("Total Happiness: {}".format(calculate_happiness(D, G)))
-#     write_output_file(D, 'out/test.out')
+if __name__ == '__main__':
+    assert len(sys.argv) == 2
+    path = sys.argv[1]
+    G, s = read_input_file(path)
+    D = read_output_file('samples/10.out', G, s)
+    print("Total Happiness: {}".format(calculate_happiness(D, G)))
+    # write_output_file(D, 'out/test.out')
 
 
 # For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
@@ -42,3 +41,15 @@ def solve(G, s):
 #         assert is_valid_solution(D, G, s, k)
 #         cost_t = calculate_happiness(T)
 #         write_output_file(D, output_path)
+
+#pass in 2d array of outputs where each line represents a room,
+# returns a list of tuples where each tuple represents a line in the input
+def backtrack(G):
+    
+
+
+
+
+
+
+
