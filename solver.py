9ce81000014df4b496879463e0716e21d3c337bc
Vaibhav Gattani <vaibg@berkeley.edu>
2020-11-29 12:14:34 -0800

    Added submission fileand minor changes to starter code

--- solver.py
+++ solver.py
@@ -2,6 +2,8 @@ import networkx as nx
 from parse import read_input_file, write_output_file
 from utils import is_valid_solution, calculate_happiness
 import sys
+from os.path import basename, normpath
+import glob
 
 
 def solve(G, s):
@@ -13,8 +15,6 @@ def solve(G, s):
         D: Dictionary mapping for student to breakout room r e.g. {0:2, 1:0, 2:1, 3:2}
         k: Number of breakout rooms
     """
-
-    # TODO: your code here!
     pass
 
 
@@ -29,16 +29,16 @@ def solve(G, s):
 #     D, k = solve(G, s)
 #     assert is_valid_solution(D, G, s, k)
 #     print("Total Happiness: {}".format(calculate_happiness(D, G)))
-#     write_output_file(D, 'out/test.out')
+#     write_output_file(D, 'outputs/small-1.out')
 
 
 # For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
 # if __name__ == '__main__':
-#     inputs = glob.glob('file_path/inputs/*')
+#     inputs = glob.glob('inputs/*')
 #     for input_path in inputs:
-#         output_path = 'file_path/outputs/' + basename(normpath(input_path))[:-3] + '.out'
-#         G, s = read_input_file(input_path, 100)
+#         output_path = 'outputs/' + basename(normpath(input_path))[:-3] + '.out'
+#         G, s = read_input_file(input_path)
 #         D, k = solve(G, s)
 #         assert is_valid_solution(D, G, s, k)
-#         cost_t = calculate_happiness(T)
+#         happiness = calculate_happiness(D, G)
 #         write_output_file(D, output_path)
